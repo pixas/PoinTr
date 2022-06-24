8382437f70273ebacf63acca627f282acce714fe
pixas <jiangshuyang@sjtu.edu.cn>
2022-06-24 12:28:36 +0800

    less point input more point output

--- models/dgcnn_group.py
+++ models/dgcnn_group.py
@@ -88,7 +88,7 @@ class DGCNN_Grouper(nn.Module):
         f = self.layer1(f)
         f = f.max(dim=-1, keepdim=False)[0]
 
-        coor_q, f_q = self.fps_downsample(coor, f, 1024)
+        coor_q, f_q = self.fps_downsample(coor, f, 1536)
         # coor_q, f_q = coor, f
         f = self.get_graph_feature(coor_q, f_q, coor, f)
         f = self.layer2(f)
@@ -99,7 +99,7 @@ class DGCNN_Grouper(nn.Module):
         f = self.layer3(f)
         f = f.max(dim=-1, keepdim=False)[0]
 
-        coor_q, f_q = coor, f
+        coor_q, f_q = self.fps_downsample(coor, f, 1024)
         f = self.get_graph_feature(coor_q, f_q, coor, f)
         f = self.layer4(f)
         f = f.max(dim=-1, keepdim=False)[0]
